5668d8b434f649fcf4d45c25f9b324fa67fb13ca
Melanie Martins <melanie.martins@aluno.senai.br>
2026-03-20 11:09:51 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 const express = require('express');
 const cors = require('cors');
-
 // Importando nossos Middlewares customizados
 const logger = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
-
 const app = express();
 
 // Middlewares Globais
 app.use(cors());
 app.use(express.json());
-
 // Nosso Middleware de Log!
 app.use(logger);
-
 // Rota de teste
 app.get('/', (req, res) => {
     res.json({ mensagem: '🍣 Bem-vindo a API do Haruy Sushi!' });
@@ -26,12 +22,10 @@ const rotasProdutos = require('./routes/produtos');
 // Utilizando rotas
 app.use('/api/categorias', rotasCategorias);
 app.use('/api/produtos', rotasProdutos);
-
 // Tratamento de Rotas não encontradas (Erro 404)
 app.use((req, res, next) => {
     res.status(404).json({ mensagem: "Rota não encontrada na API do Haruy Sushi." });
 });
-
 // MIDDLEWARE DE ERROS GLOBAL (Sempre no final!)
 app.use(errorHandler);
 
@@ -53,10 +47,6 @@ app.listen(PORTA, () => {
     console.log( `POST  /api/produtos`);
     console.log( `PUT   /api/produtos/:id`);
     console.log( `DELETE /api/produtos/:id`);
-    console.log('');
-    console.log('💣 Rota de teste de erro:');
-    console.log( `GET   /api/produtos/erro-teste`);
-    console.log('');
 });
 
 module.exports = app;
